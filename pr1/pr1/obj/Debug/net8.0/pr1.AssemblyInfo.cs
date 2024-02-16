e6ab677509bf6421dc619956c08e4079c7a267e5
libarb <pontiuspilate36@gmail.com>
2024-02-16 10:09:12 +0300

    1

--- pr1/pr1/obj/Debug/net8.0/pr1.AssemblyInfo.cs
+++ pr1/pr1/obj/Debug/net8.0/pr1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pr1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a26455aaea1b8cee1d1d6aca0c8ff18ec097adc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("pr1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pr1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
